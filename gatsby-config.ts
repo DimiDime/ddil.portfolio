91385166a9785fd45b72c76c4014f796fca1cabc
DimiDime <ilev997@gmail.com>
2025-07-22 18:45:07 +0200

    Init3

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Portfolio Dimitar`,
+    siteTitleAlt: `Portfolio`,
+    siteHeadline: `portfolio`,
+    siteUrl: `https://dimitar.portfolio.com`,
+    siteDescription: `This is my portfolio website`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `Dimitar`,
   },
   trailingSlash: `always`,
   plugins: [
